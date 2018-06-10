9876b5702ea4c66d6cacd3f15f455b8259c9429d
Jaddue <jaddue@icloud.com>
2018-06-10 15:35:43 +0900

    레이아웃 설계 v1.0

---
+++ vue-projobs/src/components/ProjobsHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        <h1>프로이직러 프로젝트!</h1>
+    </header>
+</template>
+
+<style scoped>
+ h1 {
+     color:#2f3b52;
+     font-weight:900;
+     margin:2.5rem 0 1.5rem;
+ }
+</style>
+
